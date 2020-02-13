b526d145a0765393f75850ed301a9b80c3baaa96
kulvirvirk <kvirkdev@gmail.com>
2020-02-13 01:40:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Excersise: 
+# 1. ask username and password from user
+# 2. print password and it's length
+# 3. also, cover the password with * 
+
+
+username = input('Hi there, enter your username: ')
+password = input('Enter your password: ')
+
+password_length = len(password)
+covered_password = '*' * password_length
+print(f'Hi {username}, your {covered_password} is {password_length} char(s) long!')
